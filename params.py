d504a102ca58bb4fee0ccca6a8b54358bf55725e
Oriol Mont <58004352+oriolmontcreus@users.noreply.github.com>
2023-05-31 12:26:14 +0200

    Update params.py

--- params.py
+++ params.py
@@ -1,4 +1,4 @@
-atlas_connection_link = "mongodb+srv://omontdam:tYNzOIOfxV0qGe9E@m06-mongodb.lyi4ugy.mongodb.net/?retryWrites=true&w=majority"
-api_key = "544f54047517729ab047691401823938"
+atlas_connection_link = "{atlas_link}"
+api_key = "{api_key}"
 menu_options = ['Consult the classification', 'List the results of matches on a date', 'List all teams in the League', 'Quit application']
 menu_options_yes_no = ['Yes', 'No']
